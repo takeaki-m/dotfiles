6222f70fe3ad62ddb92ab92e199c320147e31b97
TakeakiMiyazawa <takeaki@mba.local>
2021-08-07 13:00:10 +0900

    mod brew cask install and add dash app at cask-app-list

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -14,60 +14,60 @@ echo "ok. run brew upgrade..."
 brew upgrade -all
 
 formulas=(
-anyenv
-apr
-apr-util
-autoconf
-bash-completion
-berkeley-db
-cask
-cmake
-cscope
-gcc
-gdbm
-gettext
-git
-gmp
-go
-icu4c
-isl
-libevent
-libmpc
-libssh2
-libyaml
-lua
-lz4
-mono
-mpdecimal
-mpfr
-mysql
-ncurses
-node
-openssl@1.1
-pcre2
-perl
-pkg-config
-protobuf
-pwgen
-pyenv
-python@3.8
-python@3.9
-readline
-ruby
-rust
-sqlite
-subversion
-tcl-tk
-tmux
-tree
-utf8proc
-vim
-xz
-zsh-autosuggestions
-zsh-completions
-zsh-git-prompt
-zsh-syntax-highlighting
-alfred
+    anyenv
+    apr
+    apr-util
+    autoconf
+    bash-completion
+    berkeley-db
+    cask
+    cmake
+    cscope
+    gcc
+    gdbm
+    gettext
+    git
+    gmp
+    go
+    icu4c
+    isl
+    libevent
+    libmpc
+    libssh2
+    libyaml
+    lua
+    lz4
+    mono
+    mpdecimal
+    mpfr
+    mysql
+    ncurses
+    node
+    openssl@1.1
+    pcre2
+    perl
+    pkg-config
+    protobuf
+    pwgen
+    pyenv
+    python@3.8
+    python@3.9
+    readline
+    ruby
+    rust
+    sqlite
+    subversion
+    tcl-tk
+    tmux
+    tree
+    utf8proc
+    vim
+    xz
+    zsh-autosuggestions
+    zsh-completions
+    zsh-git-prompt
+    zsh-syntax-highlighting
+    alfred
 )
 
 echo "start brew install apps..."
@@ -76,19 +76,18 @@ for formula in "${formulas[@]}"; do
 done
 
 casks=(
-dropbox
-google-chrome
-slack
-alfred
-iterm2
-virtualbox
-vagrant
-vagrant-manager
+    slack
+    alfred
+    iterm2
+    virtualbox
+    vagrant
+    vagrant-manager
+    dash
 )
 
 echo "start brew cask install apps..."
 for cask in "${cask[@]}"; do
-    brew cask install $cask
+    brew  install -cask $cask
 done
 
 brew cleanup
