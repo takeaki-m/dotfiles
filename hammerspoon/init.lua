32a5bde35381d5b7a85646a3a19ec834ed32671e
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2023-06-24 15:22:37 +0900

    feat: 🎸 add init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,22 +6,23 @@ units = {
   left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
-  botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
-  upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
-  botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
+  upright     = { x = 0.50, y = 0.00, w = 0.50, h = 0.50 },
+  botright    = { x = 0.50, y = 0.50, w = 0.50, h = 0.50 },
+  upleft      = { x = 0.00, y = 0.00, w = 0.50, h = 0.50 },
+  botleft     = { x = 0.00, y = 0.50, w = 0.50, h = 0.50 },
   maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
 }
 
 mash = { 'ctrl', 'cmd' }
+--mash = { 'ctrl', 'space' }
 hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
 hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
 hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
 hs.hotkey.bind(mash, 'j', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
-hs.hotkey.bind(mash, ']', function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
-hs.hotkey.bind(mash, '[', function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
-hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
-hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
+hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.upright,  nil, true) end)
+hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.upleft,   nil, true) end)
+hs.hotkey.bind(mash, '.', function() hs.window.focusedWindow():move(units.botleft,  nil, true) end)
+hs.hotkey.bind(mash, '/', function() hs.window.focusedWindow():move(units.botright, nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
 hs.hotkey.bind(mash, 'n', function() hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():next(), false, true, 0) end)
 hs.hotkey.bind(mash, 'p', function() hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():previous(), false, true, 0) end)
