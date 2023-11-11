bd7aa65fa130eacca704717dd941f8877177f738
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2023-11-11 12:54:22 +0900

    feat: add check if .gitconfig file exits
    - if file isn't exist, make .gitconfig file

--- gitconfig/git_setup.sh
+++ gitconfig/git_setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "### check if .gitconfig exists ###"
+if [ -e ~/.gitconfig ]; then
+  echo ".gitconfig file exists. continue to settings"
+else
+  echo ".gitconfig does not exists. make .gitconfig file and continue to settings"
+  touch ~/.gitconfig
+fi
+
 echo "### start .gitconfig settings ###"
 git config --global include.path `readlink -f gitconfig/.gitconfig_shared`
 
