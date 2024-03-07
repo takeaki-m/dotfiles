56ba36abfce5c10a689e698c46726c4edeea4136
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2024-03-07 20:14:26 +0900

    feat(ide-vim): udpate linking setup shell
    introduce loops to make multi file links

--- ide_vim_setup.sh
+++ ide_vim_setup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-IDEA_DOT_FILE=.ideavimrc
-echo "make $ECLIPSE_DOT_FILE synbolik link"
-ln -s $HOME/settings/dotfiles/$IDEA_DOT_FILE $HOME/$IDEA_DOT_FILE
 
+VIM_DOT_FILE_HOME_PATH=$HOME/settings/dotfiles
+IDE_DOT_FILE=.ide-vimrc
+IDEA_DOT_FILE=.ideavimrc
 ECLIPSE_DOT_FILE=.vrapperrc
-echo "make $ECLIPSE_DOT_FILE synbolik link"
-ln -s $HOME/settings/dotfiles/$ECLIPSE_DOT_FILE $HOME/$ECLIPSE_DOT_FILE
+
+MESSAGE_CHECK_FILE="ファイルの存在チェック。存在しなければ作成します"
+MESSAGE_MAKE_FILE="ファイルが存在しませんでした。作成します"
+MESSAGE_FILE_EXIST="ファイルが存在します"
+
+echo "$MESSAGE_CHECK_FILE"
+for file in $IDEA_DOT_FILE $ECLIPSE_DOT_FILE $IDE_DOT_FILE
+do
+	if [ ! -e $HOME/$file ]; then
+		echo "$file $MESSAGE_MAKE_FILE"
+		ln -s $VIM_DOT_FILE_HOME_PATH/$file $HOME/$file
+	else
+		echo "$file $MESSAGE_FILE_EXIST"
+	fi
+done
+
