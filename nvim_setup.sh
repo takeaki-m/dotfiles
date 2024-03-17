df85d30ce530b8fba9437cf3498418d0b11424a0
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2024-03-17 12:25:10 +0900

    feat(nvim): add nvim package manger pckr

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -3,9 +3,11 @@
 NVIM_DOT_FILE=init.lua
 NVIM_DOT_FILE_HOME_PATH=$HOME/settings/dotfiles/nvim
 NVIM_DOT_FILE_LUA_PATH=$NVIM_DOT_FILE_HOME_PATH/lua
+NVIM_DOT_FILE_LUA_PLUGINS_PATH=$NVIM_DOT_FILE_LUA_PATH/plugins
 
 NVIM_HOME_PATH=$HOME/.config/nvim
 NVIM_LUA_PATH=$NVIM_HOME_PATH/lua
+NVIM_LUA_PLUGIN_PATH=$NVIM_LUA_PATH/plugins
 
 MESSAGE_CHECK_DIRECTORY="ディレクトリの存在チェック。存在しなければ作成します"
 MESSAGE_MAKE_DIRECTORY="ディレクトリが存在しませんでした。作成します"
@@ -15,9 +17,7 @@ MESSAGE_CHECK_FILE="ファイルの存在チェック。存在しなければ作
 MESSAGE_MAKE_FILE="ファイルが存在しませんでした。作成します"
 MESSAGE_FILE_EXIST="ファイルが存在します"
 
-
-for dist in $NVIM_HOME_PATH $NVIM_LUA_PATH
-do
+for dist in $NVIM_HOME_PATH $NVIM_LUA_PATH $NVIM_LUA_PLUGIN_PATH; do
 	echo "$dist $MESSAGE_CHECK_DIRECTORY"
 	if [ ! -e $dist ]; then
 		echo "$dist $MESSAGE_MAKE_DIRECTORY"
@@ -27,7 +27,6 @@ do
 	fi
 done
 
-
 echo "$NVIM_DOT_FILE $MESSAGE_CHECK_FILE"
 if [ ! -e $NVIM_HOME_PATH/$NVIM_DOT_FILE ]; then
 	echo "$NVIM_DOT_FILE $MESSAGE_MAKE_FILE"
@@ -36,8 +35,7 @@ else
 	echo "$NVIM_DOT_FILE $MESSAGE_FILE_EXIST"
 fi
 
-for file in options.lua keymaps.lua
-do
+for file in options.lua keymaps.lua; do
 	if [ ! -e $NVIM_LUA_PATH/$file ]; then
 		echo "$file $MESSAGE_MAKE_FILE"
 		ln -s $NVIM_DOT_FILE_LUA_PATH/$file $NVIM_LUA_PATH/$file
@@ -46,3 +44,14 @@ do
 	fi
 done
 
+for file in "$NVIM_DOT_FILE_LUA_PLUGINS_PATH"/*.lua; do
+	## $file変数のままではfull pathになってしまうため、ファイル名だけ抜き出す
+	echo "$file"
+	filename=$(basename "$file")
+	if [ ! -e $NVIM_LUA_PLUGIN_PATH/$filename ]; then
+		echo "$filename $MESSAGE_MAKE_FILE"
+		ln -s $NVIM_DOT_FILE_LUA_PLUGINS_PATH/$filename $NVIM_LUA_PLUGIN_PATH/$filename
+	else
+		echo "$filename $MESSAGE_FILE_EXIST"
+	fi
+done
