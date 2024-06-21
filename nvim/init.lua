749be331e5d3b7ea9bb0ce72ed160e03dac2f59c
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2024-06-22 00:05:49 +0900

    feat(nvim): install lsp packages

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,6 +42,16 @@ require('pckr').add{
   'lewis6991/gitsigns.nvim';
   'nvim-lualine/lualine.nvim';
   'kdheepak/lazygit.nvim';
+  'neovim/nvim-lsp';
+  'neovim/nvim-lspconfig';
+  'williamboman/mason.nvim';
+  'williamboman/mason-lspconfig.nvim';
+  'L3MON4D3/LuaSnip';
+
+  'hrsh7th/nvim-cmp';
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/cmp-buffer';
+  'saadparwaiz1/cmp_luasnip';
   -- colortheme
   'rose-pine/neovim';
   'craftzdog/solarized-osaka.nvim';
@@ -49,3 +59,39 @@ require('pckr').add{
 -- activate indent-blankline.nvim
 require('ibl').setup()
 require('lualine').setup()
+-- lsp
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function(server_name)
+    require("lspconfig")[server_name].setup {}
+  end,
+}
+-- lspのハンドラーに設定
+CAPABILITIES = require("cmp_nvim_lsp").default_capabilities()
+
+-- lspの設定後に追加
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -59,3 +59,34 @@ end
 
 vim.api.nvim_set_keymap('n', '<Leader>rp', ':lua PasteCommandOutput("readlink -f " .. vim.fn.expand("%"))<CR>', {noremap = true, silent = true})
 
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+keymap('n', '<space>e', '<cmd>:lua vim.diagnostic.open_float()<CR>', opts)
+keymap('n', '[d', '<cmd>:lua vim.diagnostic.goto_prev()<CR>', opts)
+keymap('n', ']d', '<cmd>:lua vim.diagnostic.goto_next()<CR>', opts)
+
+
+-- LSPサーバアタッチ時の処理
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ctx)
+    keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  end,
+})
+
