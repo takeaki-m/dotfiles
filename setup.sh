7523cd826e38a2b1721e57a45b49739b1086d07a
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2024-12-09 20:01:12 +0900

    fix: exclude homebrew from all set up shell

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DOTFILES_ROOT_PATH=$HOME/settings/dotfiles
 
 echo " 各種設定ファイルを呼び出してdotifilesの設定を行います"
 sh $DOTFILES_ROOT_PATH/dotfile_setup.sh
-sh $DOTFILES_ROOT_PATH/homebrew_install.sh
+#sh $DOTFILES_ROOT_PATH/homebrew_install.sh
 sh $DOTFILES_ROOT_PATH/zsh/zsh_setup.sh
 sh $DOTFILES_ROOT_PATH/karabiner/karabiner_setup.sh
 sh $DOTFILES_ROOT_PATH/gitconfig/git_setup.sh
