2bd8b708014dd223bd6a0089e559804118070fcf
Takeaki Miyazawa <tmiyazawa@netprotections.co.jp>
2025-01-08 12:47:14 +0900

    feat(zsh): add function search local git repos

--- zsh/global_setting.zsh
+++ zsh/global_setting.zsh
@@ -56,6 +56,18 @@ fi
 #    autoload -Uz compinit
 #    compinit
 #fi
+#
+## ghqとの連携。ghqの管理化にあるリポジトリを一覧表示する。ctrl - ]にバインド。
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco --prompt="repositories >" --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-src
+bindkey '^]' peco-src
 
 ## prompt options
 GIT_PS1_SHOWDIRTYSTATE=true
