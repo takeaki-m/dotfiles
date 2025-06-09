86b2cca4379327af32c82435ce8f6848299a49ed
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-06-09 19:15:20 +0900

    feat(nvim): open with default colorscheme-catppuccin-mocha

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,5 +1,5 @@
 --vim.cmd("colorscheme defalut")
-vim.cmd ("colorscheme sorbet")
+--vim.cmd ("colorscheme sorbet")
+--vim.cmd("colorscheme tokyonight-night")
 --vim.cmd ("colorscheme rose-pine")
---vim.cmd ("colorscheme lunaperche")-- suite for light theme
-
+vim.cmd ("colorscheme catppuccin-mocha")-- suite for light theme
