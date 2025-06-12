37dee76a86e1eaed178233503f98f92b218fbb88
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-06-12 11:12:39 +0900

    feat(nvim): set default colorscheme to kanagawa

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -3,5 +3,6 @@
 --vim.cmd("colorscheme tokyonight-night")
 --vim.cmd ("colorscheme rose-pine")
 --vim.cmd ("colorscheme catppuccin-mocha")-- suite for light theme
-vim.cmd ("colorscheme nightfox")-- suite for light theme
+--vim.cmd ("colorscheme nightfox")-- suite for light theme
+vim.cmd ("colorscheme kanagawa")-- suite for light theme
 
