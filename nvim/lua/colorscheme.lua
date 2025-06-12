ddb02593159cdf175ff1aa8996fcb6c9dace2c93
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-06-12 10:15:17 +0900

    feat(nvim): change default colorscheme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -2,4 +2,6 @@
 --vim.cmd ("colorscheme sorbet")
 --vim.cmd("colorscheme tokyonight-night")
 --vim.cmd ("colorscheme rose-pine")
-vim.cmd ("colorscheme catppuccin-mocha")-- suite for light theme
+--vim.cmd ("colorscheme catppuccin-mocha")-- suite for light theme
+vim.cmd ("colorscheme nightfox")-- suite for light theme
+
