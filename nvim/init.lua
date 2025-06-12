654b9ecd435f49b9b36e0a5bd03caef67c438c93
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-06-12 16:44:01 +0900

    feat(nvim): show hidden files at live_grep

--- nvim/init.lua
+++ nvim/init.lua
@@ -208,5 +208,10 @@ require("telescope").setup({
     find_files = {
       hidden = true,
     },
+    live_grep = {
+      additional_args = function()
+        return { "--hidden" }
+      end
+    },
   },
 })
