e8ccc449a547efa1aa762cc6e63f3585adf870de
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-07-26 16:41:50 +0900

    feat(calude): setup.sh

---
+++ claude/claude_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CLAUDE_SETTINGS_FOLDER_PATH="$HOME/settings/dotfiles/claude/"
+
+ln -s $CLAUDE_SETTINGS_FOLDER_PATH/settings.json $HOME/.claude/settings.json
+ln -s $CLAUDE_SETTINGS_FOLDER_PATH/CLAUDE.md $HOME/.claude/CLAUDE.md
+
