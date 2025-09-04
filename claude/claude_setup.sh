f177d93d3d8156e51ad4d4a8a8b2c8042c7a014e
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-09-04 16:13:17 +0900

    fix(claude): setting up shell path

--- claude/claude_setup.sh
+++ claude/claude_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLAUDE_SETTINGS_FOLDER_PATH="$HOME/settings/dotfiles/claude/"
+CLAUDE_SETTINGS_FOLDER_PATH="$HOME/settings/dotfiles/claude"
 
 ln -s $CLAUDE_SETTINGS_FOLDER_PATH/settings.json $HOME/.claude/settings.json
 ln -s $CLAUDE_SETTINGS_FOLDER_PATH/CLAUDE.md $HOME/.claude/CLAUDE.md
