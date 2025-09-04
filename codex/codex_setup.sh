a4534c0c394c2aa23763269ea50f2e843b301b85
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-09-04 16:13:01 +0900

    feat(codex): codex basic settings

---
+++ codex/codex_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CODEX_SETTINGS_FOLDER_PATH="$HOME/settings/dotfiles/codex"
+
+ln -s $CODEX_SETTINGS_FOLDER_PATH/config.toml $HOME/.codex/config.toml
+ln -s $CODEX_SETTINGS_FOLDER_PATH/AGENTS.md $HOME/.codex/AGENTS.md
+
