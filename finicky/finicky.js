bbbdc5d956f6a9f691aa9780ac8884712df6ae1d
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-09-17 18:36:41 +0900

    feat(finicky): change work hours

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -7,7 +7,7 @@ export default {
                 const day = d.getDay(); // 0=日,6=土
                 const hour = d.getHours();
                 const isWeekday = day >= 1 && day <= 5;
-                const inBizHours = hour >= 9 && hour < 18;
+                const inBizHours = hour >= 6 && hour < 20;
                 return isWeekday && inBizHours;
             },
             browser: "Google Chrome"
