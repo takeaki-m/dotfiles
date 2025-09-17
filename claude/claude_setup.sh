7c7ee8b6e09bd6e7e9d40b5a50f58bf53d5bd4be
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-09-17 18:34:52 +0900

    feat(claude): settings mcp servers with cli

--- claude/claude_setup.sh
+++ claude/claude_setup.sh
@@ -5,3 +5,8 @@ CLAUDE_SETTINGS_FOLDER_PATH="$HOME/settings/dotfiles/claude"
 ln -s $CLAUDE_SETTINGS_FOLDER_PATH/settings.json $HOME/.claude/settings.json
 ln -s $CLAUDE_SETTINGS_FOLDER_PATH/CLAUDE.md $HOME/.claude/CLAUDE.md
 
+# MCP Servers
+claude mcp add awslabs_aws-documentation -- /opt/homebrew/bin/uvx awslabs.aws-documentation-mcp-server@latest
+claude mcp add mui-mcp -- npx -y @mui/mcp@latest
+claude mcp add terraform --  docker run -i --rm hashicorp/terraform-mcp-server
+
