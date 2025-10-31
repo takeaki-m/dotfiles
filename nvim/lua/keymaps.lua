6d4bbe6fb9fcc630180959d12f291683ca2335fe
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-10-31 23:01:02 +0900

    feat(nvim): change esc keymap in nvim termnal

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -106,7 +106,7 @@ vim.cmd([[
 -- コマンドラインモードでc-n,c-pでも補完を有効にするために方向キーに割り当てる
 --keymap('c', '<C-p>', '<Up>', opts)
 --keymap('c', '<C-n>', '<Down>', opts)
-keymap("t", "<C-g>", "<C-\\><C-n>", opts)
+keymap("t", "<C-]>", "<C-\\><C-n>", opts)
 
 keymap('n', '<Leader>lg', ':LazyGit<CR>', opts)
 -- move to specified line with <CR> insted of G
