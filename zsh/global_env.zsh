dcf61ba23a658984742eb3b8335cdc5c65db84b4
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-10-31 23:00:30 +0900

    docs(zsh): comment about deactive EDITOR for claude code nvim extension

--- zsh/global_env.zsh
+++ zsh/global_env.zsh
@@ -1,5 +1,12 @@
 # editor
-#export EDITOR=vim
+# 以下のエディタの設定でnvimを指定すると、不備があるため無効化する
+# nvimの拡張機能でclaude codeのpronput入力を外部エディタで実行する機能がある。
+# この機能では、EDITORに指定されているエディタが自動的に起動する。
+# だが一度起動すると、claude codeの拡張機能での入力に不備が出るため、入力を無効化する
+# たとえば以下の不具合など
+# - nvimでterminalのinsert modeからぬけるctrl-]を押すと、文字が入力される。
+# - チャット入力欄が画面の最下部から、上部に移動してしまう。またこれまでのchat履歴の上に重なるように表示される
+#export EDITOR=nvim
 #export CVSEDITOR="${EDITOR}"
 #export SVN_EDITOR="${EDITOR}"
 #export GIT_EDITOR="${EDITOR}"
