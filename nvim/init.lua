e9c5c9ea01f9fe55a07b68fa06293d4be3d676a0
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-10-31 22:59:18 +0900

    feat(nvim): activate tree-sitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -302,3 +302,4 @@ require("nvim-tree").setup({
     },
   },
 })
+require("nvim-treesitter").setup()
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -87,3 +87,12 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
   end,
 })
 
+--開いたbufferをトリガーにして、treesitterを有効化する
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+  callback = function (ctx)
+    -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
+    -- `pcall`でエラーを無視することでパーサーやクエリがあるかどうかを気にしなくて済む
+    pcall(vim.treesitter.start)
+  end,
+})
