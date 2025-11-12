bfd9c96fc452bd09f2eef8b929404ac5dec1cd4e
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-11-12 10:08:51 +0900

    feat(nvim): remove lua statusline items

--- nvim/init.lua
+++ nvim/init.lua
@@ -102,57 +102,19 @@ require("nvim-surround").setup()
 --require('ibl').setup()
 require('lualine').setup {
   options = {
-    icons_enabled = true,
+    icons_enabled = false, --アイコンを無効にする
     theme = 'auto',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-      refresh_time = 16, -- ~60fps
-      events = {
-        'WinEnter',
-        'BufEnter',
-        'BufWritePost',
-        'SessionLoadPost',
-        'FileChangedShellPost',
-        'VimResized',
-        'Filetype',
-        'CursorMoved',
-        'CursorMovedI',
-        'ModeChanged',
-      },
-    }
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_b = { 'branch' },
     lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
+    lualine_x = { 'filetype' }, -- encoding formatを削除
+    lualine_y = { }, -- progressを削除
     lualine_z = { 'location' }
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
 }
 -- lsp
 require("mason").setup()
