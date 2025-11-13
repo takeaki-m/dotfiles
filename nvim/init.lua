d61dde7778e715383410cc0cb4ef9a89dcfc3a50
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-11-13 10:48:22 +0900

    fix(nvim): remove branch name from lua status line

--- nvim/init.lua
+++ nvim/init.lua
@@ -119,7 +119,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
+    lualine_b = { '' },
     lualine_c = { 'filename' },
     lualine_x = { 'filetype' }, -- encoding formatを削除
     lualine_y = { }, -- progressを削除
