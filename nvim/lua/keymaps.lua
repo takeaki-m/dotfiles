83df80cf0cb895058f8e07a4a10697e24ea94010
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-12-11 10:31:19 +0900

    feat(nvim): open nvim-tree with focusing on file before buffer

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -50,7 +50,7 @@ keymap('n', '<Leader>fb', ':Telescope buffers<CR>', opts)
 -- telescope select registers
 keymap('n', '<Leader>fr', ':Telescope registers<CR>', opts)
 
-keymap('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
+keymap('n', '<C-n>', ':NvimTreeFindFileToggle<CR>', opts)
 
 -- 補完表示時のEnterで改行をしない
 keymap('i', '<CR>', 'pumvisible() ? "<C-y>" : "<CR>"', { expr = true, noremap = true })
