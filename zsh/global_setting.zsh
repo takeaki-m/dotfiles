4f129fec86966d8750d96d3f8c65c9b1c7ae9f80
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2025-12-12 18:40:38 +0900

    fix(zsh): ghコマンドの issueの検索数を100件表示に指定する

--- zsh/global_setting.zsh
+++ zsh/global_setting.zsh
@@ -148,7 +148,7 @@ gh() {
 ghdev() {
   echo "GitHub issueと関連付けてブランチを作成します">&2
   echo "Issue を選択してください">&2
-  issue_no=$(gh issue list | fzf | awk '{print $1}')
+  issue_no=$(gh issue list --limit 100 | fzf | awk '{print $1}')
   echo "選択されたIssue: $issue_no" >&2
   echo "Branch name:">&2
   read -r branch
