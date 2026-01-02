0a75fbda032e5fe0640919175aeb36ccc3ed3ddd
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2026-01-02 18:36:02 +0900

    feat(nvim): add which key

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,24 @@ local function setup_plugins()
     },
     'nvim-lua/plenary.nvim',
     {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
+    {
       'nvim-telescope/telescope.nvim',
       config = function()
         local actions = require("telescope.actions")
