a1197a03e4a8cabeb66514f61e640a28f88b1ebc
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2026-01-13 12:10:28 +0900

    fix(zsh): open obsidian shortcut

--- zsh/global_setting.zsh
+++ zsh/global_setting.zsh
@@ -70,7 +70,7 @@ today(){
 
     # obsidianのディレクトリを開くように先にディレクトリを移動する
     # 失敗時には中断する
-    cd $obsidian_home || return 1
+    cd "$obsidian_home" || return 1
 
     if check_os_theme_is_dark; then
         local colorscheme="tokyonight-night"
@@ -78,17 +78,23 @@ today(){
         local colorscheme="tokyonight-day"
     fi
 
-    if [[ -f "$file" ]]; then
-        # 既存ならそのファイルを普通に開く
-        command nvim $file \
-          -c "split $yesterday_note" \
-          -c "split $monthly_note" \
+    # 昨日分のファイル存在で分岐
+    if [[ -f "$yesterday_note" ]]; then
+        # nvimのsplitコマンドにスペースを含むパスを渡すため、エスケープが必要
+        # zshのパラメータ展開を利用
+        # //: 全てのマッチを置換
+        # // : //にて全ての置換を対象として、その後でスペースを指定
+        # \\ : 置換文字列(バックスラッシュ+スペース)
+        # NOTE; yesterday_noteを指定しているが、splitで開くことができない。もし実現方法がわかれば修正する
+        local escaped_yesterday="${yesterday_note// /\\ }"
+        command nvim "$monthly_note" \
+          -c "split $escaped_yesterday" \
+          -c "ObsidianToday" \
           -c "colorscheme $colorscheme"
     else
-        # 未作成なら nvim を開いて専用コマンドを実行
-        command nvim $monthly_note \
+        # 昨日分のファイルが存在しなければ、今日のファイルのみ開く
+        command nvim "$monthly_note" \
           -c "ObsidianToday" \
-          -c "split $yesterday_note" \
           -c "colorscheme $colorscheme"
     fi
 }
