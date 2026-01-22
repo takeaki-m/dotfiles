6e57313f64624e7df81b0a9322d92266b18a8b11
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2026-01-22 15:46:46 +0900

    fix(nvim): disabled which-key nvim because of c-o keymap not working

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,24 +53,25 @@ local function setup_plugins()
       end
     },
     'nvim-lua/plenary.nvim',
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
-      keys = {
-        {
-          "<leader>?",
-          function()
-            require("which-key").show({ global = false })
-          end,
-          desc = "Buffer Local Keymaps (which-key)",
-        },
-      },
-    },
+    -- insert modeでのCtrl-Oが動作しなくなるためコメントアウトする
+    -- {
+    --   "folke/which-key.nvim",
+    --   event = "VeryLazy",
+    --   opts = {
+    --     -- your configuration comes here
+    --     -- or leave it empty to use the default settings
+    --     -- refer to the configuration section below
+    --   },
+    --   keys = {
+    --     {
+    --       "<leader>?",
+    --       function()
+    --         require("which-key").show({ global = false })
+    --       end,
+    --       desc = "Buffer Local Keymaps (which-key)",
+    --     },
+    --   },
+    -- },
     {
       'nvim-telescope/telescope.nvim',
       config = function()
