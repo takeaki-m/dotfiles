143bc5d47d9534a83b5d6d23dd76d2466d6eb938
Takeaki Miyazawa <takeakimiyazawa0614@gmail.com>
2026-08-03 09:36:22 +0900

    feat(nvim): set light theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -913,3 +913,4 @@ vim.keymap.set('n', '<Leader>1', function() harpoon:list():select(1) end, { nore
 vim.keymap.set('n', '<Leader>2', function() harpoon:list():select(2) end, { noremap = true, silent = true, desc = "Harpoon file 2" })
 vim.keymap.set('n', '<Leader>3', function() harpoon:list():select(3) end, { noremap = true, silent = true, desc = "Harpoon file 3" })
 vim.keymap.set('n', '<Leader>4', function() harpoon:list():select(4) end, { noremap = true, silent = true, desc = "Harpoon file 4" })
+vim.cmd.colorscheme("flexoki-light")
